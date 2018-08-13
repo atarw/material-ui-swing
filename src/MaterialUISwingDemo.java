c765690e5c4178c9b9ace307981187412edcd0ac
Vincenzo Palazzo <vinci96-@live.it>
2018-08-13 11:48:45 +0200

    github conflict resolution

--- src/MaterialUISwingDemo.java
+++ src/MaterialUISwingDemo.java
@@ -3,7 +3,6 @@ import mdlaf.animation.MaterialUIMovement;
 import mdlaf.resources.MaterialColors;
 
 import javax.swing.JButton;
-import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -11,19 +10,9 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
-import javax.swing.JPasswordField;
-import javax.swing.JRadioButton;
-import javax.swing.JScrollPane;
 import javax.swing.JProgressBar;
 import javax.swing.JSlider;
-import javax.swing.JSpinner;
-import javax.swing.JTabbedPane;
-import javax.swing.JTable;
-import javax.swing.JTextField;
-import javax.swing.JToggleButton;
 import javax.swing.JToolBar;
-import javax.swing.JTree;
-import javax.swing.SpinnerListModel;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
@@ -101,7 +90,7 @@ public class MaterialUISwingDemo {
 
 		// add everything to the frame
 		frame.add (bar, BorderLayout.PAGE_START);
-		// frame.add (content, BorderLayout.CENTER);
+		frame.add (content, BorderLayout.CENTER);
 
 		// start animating!
 		// here, 'gray' is the color that the JComponent will transition to when the user hovers over it
@@ -112,46 +101,7 @@ public class MaterialUISwingDemo {
 		// there will be 5 intermediate colors displayed in the transition from the original components color to the new one specified
 		// the "frame rate" of the transition will be 1000 / 30, or 30 FPS
 		// the animation will take 5 * 1000 / 30 = 166.666... milliseconds to complete
-<<<<<<< refs/remotes/upstream/master
-		MaterialUIMovement.add (button, MaterialColors.LIGHT_BLUE_500, 5, 1000 / 30);
-
-		//
-		content.add (new JCheckBox ("checkbox"));
-		content.add (new JComboBox<String> (new String[]{"a", "b", "c"}));
-		content.add (new JLabel ("label"));
-		content.add (new JPasswordField ("password"));
-		content.add (new JRadioButton ("radio button"));
-		content.add (new JSlider (JSlider.HORIZONTAL, 0, 5, 2));
-		content.add (new JSpinner (new SpinnerListModel (new String[]{"d", "e", "f"})));
-		content.add (new JTable (new String[][]{{"a", "b", "c"}, {"d", "e", "f"}}, new String[]{"r", "e"}));
-		content.add (new JTextField ("text field"));
-		content.add (new JToggleButton ("toggle"));
-
-		JToolBar tb = new JToolBar ("toolbar");
-		tb.add (new JButton ("f"));
-		tb.addSeparator ();
-		tb.add (new JButton ("e"));
-		tb.setFloatable (true);
-		content.add (tb);
-
-		JTree tree = new JTree (new String[]{"a", "b"});
-		tree.setEditable (true);
-
-		content.add (tree);
-
-		JScrollPane sp = new JScrollPane (content);
-		sp.setHorizontalScrollBarPolicy (JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-		sp.setVerticalScrollBarPolicy (JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-
-		JTabbedPane tp = new JTabbedPane ();
-		tp.addTab ("bleh1", new JPanel ());
-		tp.addTab ("bleh", sp);
-
-		frame.add (tp, BorderLayout.CENTER);
-		//
-=======
 		MaterialUIMovement.add (button, MaterialColors.BLUE_400, 5, 1000 / 30);
->>>>>>> fix support for jscrollbar
 
 		// make everything visible to the world
 		frame.pack ();
