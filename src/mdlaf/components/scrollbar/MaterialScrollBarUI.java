2c8d77922018322b9091411708638ea0ffa4aa2f
Vincenzo Palazzo <vinci96-@live.it>
2018-08-13 11:50:53 +0200

    github conflict resolution

--- src/mdlaf/components/scrollbar/MaterialScrollBarUI.java
+++ src/mdlaf/components/scrollbar/MaterialScrollBarUI.java
@@ -1,13 +1,16 @@
 package mdlaf.components.scrollbar;
 
+import mdlaf.resources.MaterialColors;
 import mdlaf.resources.MaterialDrawingUtils;
+import mdlaf.resources.MaterialFonts;
+import mdlaf.resources.MaterialImages;
 
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JScrollBar;
-import javax.swing.UIManager;
 import javax.swing.plaf.ComponentUI;
-import javax.swing.plaf.basic.BasicArrowButton;
 import javax.swing.plaf.basic.BasicScrollBarUI;
 import java.awt.Graphics;
 
@@ -28,12 +31,9 @@ public class MaterialScrollBarUI extends BasicScrollBarUI {
 		super.installUI (c);
 
 		JScrollBar scrollBar = (JScrollBar) c;
-		scrollBar.setFont (UIManager.getFont ("ScrollBar.font"));
-		trackColor = UIManager.getColor ("ScrollBar.track");
-		thumbColor = UIManager.getColor ("ScrollBar.thumb");
-		thumbDarkShadowColor = UIManager.getColor ("ScrollBar.thumbDarkShadow");
-		thumbHighlightColor = UIManager.getColor ("ScrollBar.thumbHighlight");
-		thumbLightShadowColor = UIManager.getColor ("ScrollBar.thumbShadow");
+		scrollBar.setFont (MaterialFonts.REGULAR);
+		trackColor = MaterialColors.GRAY_200;
+		thumbColor = thumbDarkShadowColor = thumbHighlightColor = thumbLightShadowColor = MaterialColors.GRAY_300;
 	}
 
 	@Override
@@ -48,11 +48,19 @@ public class MaterialScrollBarUI extends BasicScrollBarUI {
 	 */
 	@Override
 	protected JButton createDecreaseButton (int orientation) {
-		JButton button = new BasicArrowButton (orientation);
+		JButton button = new JButton ();
+		if(DEBUG == true) System.out.println("orientation decrese: " + orientation);
+		if(orientation == 7) {
+			if(DEBUG == true) System.out.println("orientation decrese down: " + orientation);
+			button = new JButton (new ImageIcon (MaterialImages.LEFTH_ARROW));
+		}else {
+			if(DEBUG == true) System.out.println("orientation decrese right: " + orientation);
+			button = new JButton (new ImageIcon (MaterialImages.UP_ARROW));
+		}
 
 		button.setOpaque (true);
-		button.setBackground (UIManager.getColor ("ScrollBar.arrowButtonBackground"));
-		button.setBorder (UIManager.getBorder ("ScrollBar.arrowButtonBorder"));
+		button.setBackground (MaterialColors.GRAY_300);
+		button.setBorder (BorderFactory.createEmptyBorder ());
 
 		return button;
 	}
@@ -64,11 +72,18 @@ public class MaterialScrollBarUI extends BasicScrollBarUI {
 	 */
 	@Override
 	protected JButton createIncreaseButton (int orientation) {
-		JButton button = new BasicArrowButton (orientation);
-
+		JButton button = new JButton ();
+		if(DEBUG == true) System.out.println("orientation increse: " + orientation);
+		if(orientation == 5) {
+			if(DEBUG == true) System.out.println("orientation decrese up: " + orientation);
+			button = new JButton (new ImageIcon (MaterialImages.DOWN_ARROW));
+		}else {
+			if(DEBUG == true) System.out.println("orientation decrese lefth: " + orientation);
+			button = new JButton (new ImageIcon (MaterialImages.RIGHT_ARROW));
+		}
 		button.setOpaque (true);
-		button.setBackground (UIManager.getColor ("ScrollBar.arrowButtonBackground"));
-		button.setBorder (UIManager.getBorder ("ScrollBar.arrowButtonBorder"));
+		button.setBackground (MaterialColors.GRAY_300);
+		button.setBorder (BorderFactory.createEmptyBorder ());
 
 		return button;
 	}
