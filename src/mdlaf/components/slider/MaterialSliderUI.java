181be0a2d39df35ac03c0a02aa525daadd503ec5
Vincenzo Palazzo <vinci96-@live.it>
2018-08-13 11:54:50 +0200

    github conflict resolution

--- src/mdlaf/components/slider/MaterialSliderUI.java
+++ src/mdlaf/components/slider/MaterialSliderUI.java
@@ -1,127 +1,174 @@
 package mdlaf.components.slider;
 
+import java.awt.Graphics;
+
+import java.awt.Rectangle;
+import javax.swing.JComponent;
+import javax.swing.JSlider;
+import javax.swing.plaf.ComponentUI;
+import javax.swing.plaf.metal.MetalLookAndFeel;
+import javax.swing.plaf.metal.MetalSliderUI;
 import mdlaf.resources.MaterialColors;
 import mdlaf.resources.MaterialDrawingUtils;
 
-import javax.swing.JComponent;
-import javax.swing.JSlider;
-import javax.swing.UIManager;
-import javax.swing.plaf.ComponentUI;
-import javax.swing.plaf.basic.BasicSliderUI;
-import java.awt.Dimension;
-import java.awt.Graphics;
-
-public class MaterialSliderUI extends BasicSliderUI {
-
-	private static final int NORMAL_THUMB_RADIUS = 7;
-	private static final int DRAG_THUMB_RADIUS = 14;
-	private static final Dimension THUMB_SIZE = new Dimension (DRAG_THUMB_RADIUS * 2, DRAG_THUMB_RADIUS * 2);
-
-	public MaterialSliderUI (JSlider slider) {
-		super (slider);
-	}
-
-	public static ComponentUI createUI (JComponent c) {
-		return new MaterialSliderUI ((JSlider) c);
-	}
-
-	@Override
-	public void installUI (JComponent c) {
-		super.installUI (c);
-
+/**
+ * 
+ * @author https://github.com/vincenzopalazzo
+ *
+ */
+public class MaterialSliderUI extends MetalSliderUI{
+	
+	public static ComponentUI createUI(JComponent c) {
 		JSlider slider = (JSlider) c;
-		slider.setFont (UIManager.getFont ("Slider.font"));
-		slider.setBackground (UIManager.getColor ("Slider.background"));
-		slider.setForeground (UIManager.getColor ("Slider.foreground"));
-		slider.setBorder (UIManager.getBorder ("Slider.border"));
+		//return new MaterialSliderUI(slider);
+		return new MaterialSliderUI();
 	}
 
 	@Override
-	public Dimension getThumbSize () {
-		return THUMB_SIZE;
+	public void installUI(JComponent c) {
+		// TODO Auto-generated method stub
+		super.installUI(c);
+		JSlider slider = (JSlider) c;
+		slider.setBackground(MaterialColors.WHITE);
+		slider.setPaintTrack(true);
 	}
 
 	@Override
-	public void paintThumb (Graphics g) {
-		g = MaterialDrawingUtils.getAliasedGraphics (g);
-
-		int cx = thumbRect.x + thumbRect.width / 2;
-		int cy = thumbRect.y + thumbRect.height / 2;
-
-		if (isDragging ()) {
-			g.setColor (MaterialColors.bleach (slider.getForeground (), 0.5f));
-			drawCircle (g, cx, cy, DRAG_THUMB_RADIUS);
-		}
-
-		g.setColor (slider.getForeground ());
-		drawCircle (g, cx, cy, NORMAL_THUMB_RADIUS);
-
-		// need to redraw loaded part of progress line
-		Line loaded = getTrack (true);
-		g.drawLine (loaded.x1, loaded.y1, loaded.x2, loaded.y2);
+	public void paint(Graphics g, JComponent c) {
+		super.paint(MaterialDrawingUtils.getAliasedGraphics(g), c);
 	}
+	
+	/**
+	 * original code of MetalSliderUI
+	 * @return Rectangle
+	 */
 
-	@Override
-	public void paintTrack (Graphics g) {
-		g = MaterialDrawingUtils.getAliasedGraphics (g);
+	private Rectangle getPaintTrackRect() {
+        int trackLeft = 0, trackRight, trackTop = 0, trackBottom;
+        if (slider.getOrientation() == JSlider.HORIZONTAL) {
+            trackBottom = (trackRect.height - 1) - getThumbOverhang();
+            trackTop = trackBottom - (getTrackWidth() - 1);
+            trackRight = trackRect.width - 1;
+        }
+        else {
+            if (this.leftToRightCache) {
+                trackLeft = (trackRect.width - getThumbOverhang()) -
+                                                         getTrackWidth();
+                trackRight = (trackRect.width - getThumbOverhang()) - 1;
+            }
+            else {
+                trackLeft = getThumbOverhang();
+                trackRight = getThumbOverhang() + getTrackWidth() - 1;
+            }
+            trackBottom = trackRect.height - 1;
+        }
+        return new Rectangle(trackRect.x + trackLeft, trackRect.y + trackTop,
+                             trackRight - trackLeft, trackBottom - trackTop);
+    }
+	
+	/**
+	 * original code of MetalSliderUI with some tricks on the colors
+	 */
+    public void paintTrack(Graphics g)  {
+    	g.translate( trackRect.x, trackRect.y );
 
-		g.setColor (UIManager.getColor ("Slider.trackColor"));
-		Line unloaded = getTrack (false);
-		g.drawLine (unloaded.x1, unloaded.y1, unloaded.x2, unloaded.y2);
+        int trackLeft = 0;
+        int trackTop = 0;
+        int trackRight;
+        int trackBottom;
 
-		g.setColor (slider.getForeground ());
-		Line loaded = getTrack (true);
-		g.drawLine (loaded.x1, loaded.y1, loaded.x2, loaded.y2);
-	}
+        // Draw the track
+        if ( slider.getOrientation() == JSlider.HORIZONTAL ) {
+            trackBottom = (trackRect.height - 1) - getThumbOverhang();
+            trackTop = trackBottom - (getTrackWidth() - 1);
+            trackRight = trackRect.width - 1;
+        }
+        else {
+            if (this.leftToRightCache) {
+                trackLeft = (trackRect.width - getThumbOverhang()) -
+                                                         getTrackWidth();
+                trackRight = (trackRect.width - getThumbOverhang()) - 1;
+            }
+            else {
+                trackLeft = getThumbOverhang();
+                trackRight = getThumbOverhang() + getTrackWidth() - 1;
+            }
+            trackBottom = trackRect.height - 1;
+        }
 
-	private static void drawCircle (Graphics g, int x, int y, int radius) {
-		g.fillOval (x - radius, y - radius, radius * 2, radius * 2);
-	}
+        if ( slider.isEnabled() ) {
+            g.setColor(MaterialColors.LIGHT_BLUE_400 );
+            g.drawRect( trackLeft, trackTop,
+                        (trackRight - trackLeft) - 1, (trackBottom - trackTop) - 1 );
 
-	private Line getTrack (boolean loaded) {
-		if (slider.getOrientation () == JSlider.HORIZONTAL) {
-			Line left = new Line (trackRect.x, thumbRect.y + thumbRect.height / 2, thumbRect.x + thumbRect.width / 2, thumbRect.y + thumbRect.height / 2);
-			Line right = new Line (thumbRect.x + thumbRect.width / 2, thumbRect.y + thumbRect.height / 2, trackRect.x + trackRect.width, thumbRect.y + thumbRect.height / 2);
+            g.setColor( MaterialColors.LIGHT_BLUE_400 );
+            g.drawLine( trackLeft + 1, trackBottom, trackRight, trackBottom );
+            g.drawLine( trackRight, trackTop + 1, trackRight, trackBottom );
 
-			if (loaded) {
-				return slider.getInverted () ? right : left;
-			}
-			else {
-				return slider.getInverted () ? left : right;
-			}
-		}
-		else {
-			Line top = new Line (thumbRect.x + thumbRect.width / 2, trackRect.y, thumbRect.x + thumbRect.width / 2, thumbRect.y + thumbRect.height / 2);
-			Line bottom = new Line (thumbRect.x + thumbRect.width / 2, thumbRect.y + thumbRect.height / 2, thumbRect.x + thumbRect.width / 2, trackRect.y + trackRect.height);
+            g.setColor( MaterialColors.LIGHT_BLUE_400 );
+            g.drawLine( trackLeft + 1, trackTop + 1, trackRight - 2, trackTop + 1 );
+            g.drawLine( trackLeft + 1, trackTop + 1, trackLeft + 1, trackBottom - 2 );
+        }
+        else {
+            g.setColor( MetalLookAndFeel.getControlShadow() );
+            g.drawRect( trackLeft, trackTop,
+                        (trackRight - trackLeft) - 1, (trackBottom - trackTop) - 1 );
+        }
 
-			if (loaded) {
-				return slider.getInverted () ? top : bottom;
-			}
-			else {
-				return slider.getInverted () ? bottom : top;
-			}
-		}
-	}
+        // Draw the fill
+        if ( filledSlider ) {
+            int middleOfThumb;
+            int fillTop;
+            int fillLeft;
+            int fillBottom;
+            int fillRight;
 
-	@Override
-	public void paintFocus (Graphics g) {
+            if ( slider.getOrientation() == JSlider.HORIZONTAL ) {
+                middleOfThumb = thumbRect.x + (thumbRect.width / 2);
+                middleOfThumb -= trackRect.x; // To compensate for the g.translate()
+                fillTop = !slider.isEnabled() ? trackTop : trackTop + 1;
+                fillBottom = !slider.isEnabled() ? trackBottom - 1 : trackBottom - 2;
 
-	}
+                if ( !drawInverted() ) {
+                    fillLeft = !slider.isEnabled() ? trackLeft : trackLeft + 1;
+                    fillRight = middleOfThumb;
+                }
+                else {
+                    fillLeft = middleOfThumb;
+                    fillRight = !slider.isEnabled() ? trackRight - 1 : trackRight - 2;
+                }
+            }
+            else {
+                middleOfThumb = thumbRect.y + (thumbRect.height / 2);
+                middleOfThumb -= trackRect.y; // To compensate for the g.translate()
+                fillLeft = !slider.isEnabled() ? trackLeft : trackLeft + 1;
+                fillRight = !slider.isEnabled() ? trackRight - 1 : trackRight - 2;
 
-	@Override
-	public void paint (Graphics g, JComponent c) {
-		super.paint (MaterialDrawingUtils.getAliasedGraphics (g), c);
-	}
+                if ( !drawInverted() ) {
+                    fillTop = middleOfThumb;
+                    fillBottom = !slider.isEnabled() ? trackBottom - 1 : trackBottom - 2;
+                }
+                else {
+                    fillTop = !slider.isEnabled() ? trackTop : trackTop + 1;
+                    fillBottom = middleOfThumb;
+                }
+            }
 
-	private static class Line {
+            if ( slider.isEnabled() ) {
+                g.setColor( MaterialColors.LIGHT_BLUE_400 );
+                g.drawLine( fillLeft, fillTop, fillRight, fillTop );
+                g.drawLine( fillLeft, fillTop, fillLeft, fillBottom );
 
-		int x1, y1, x2, y2;
+                g.setColor(MaterialColors.LIGHT_BLUE_400  );
+                g.fillRect( fillLeft + 1, fillTop + 1,
+                            fillRight - fillLeft, fillBottom - fillTop );
+            }
+            else {
+                g.setColor( MetalLookAndFeel.getControlShadow() );
+                g.fillRect(fillLeft, fillTop, fillRight - fillLeft, fillBottom - fillTop);
+            }
+        }
 
-		Line (int x1, int y1, int x2, int y2) {
-			this.x1 = x1;
-			this.y1 = y1;
-			this.x2 = x2;
-			this.y2 = y2;
-		}
-	}
+        g.translate( -trackRect.x, -trackRect.y );
+    }
 }
