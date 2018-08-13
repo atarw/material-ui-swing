77196f320fef4fa1f37b8b743a2b16f461dc0308
Vincenzo Palazzo <vinci96-@live.it>
2018-08-13 11:55:22 +0200

    Revert "github conflict resolution"
    This reverts commit 73aee45a20f9d184afcf99ed1e46ac9da8365d55.

--- src/mdlaf/components/toolbar/MaterialToolBarUI.java
+++ src/mdlaf/components/toolbar/MaterialToolBarUI.java
@@ -1,5 +1,6 @@
 package mdlaf.components.toolbar;
 
+<<<<<<< refs/remotes/upstream/master
 import mdlaf.resources.MaterialDrawingUtils;
 
 import javax.swing.JComponent;
@@ -34,5 +35,57 @@ public class MaterialToolBarUI extends BasicToolBarUI {
 	@Override
 	public void paint (Graphics g, JComponent c) {
 		super.paint (MaterialDrawingUtils.getAliasedGraphics (g), c);
+=======
+import java.awt.Graphics;
+
+import javax.swing.BorderFactory;
+import javax.swing.JComponent;
+import javax.swing.JToolBar;
+import javax.swing.plaf.ComponentUI;
+import javax.swing.plaf.basic.BasicToolBarUI;
+import mdlaf.resources.MaterialBorders;
+import mdlaf.resources.MaterialColors;
+import mdlaf.resources.MaterialDrawingUtils;
+import mdlaf.resources.MaterialFonts;
+
+/**
+ * 
+ * @author https://github.com/vincenzopalazzo
+ *
+ */
+
+public class MaterialToolBarUI extends BasicToolBarUI{
+	
+	private static final boolean DEBUG = false;
+	
+	public static ComponentUI createUI (JComponent c) {
+		return new MaterialToolBarUI();
+	}
+
+	@Override
+	public void installUI(JComponent c) {
+		if(!(c instanceof JToolBar)) {
+			if(DEBUG) System.out.println("Ok il componente non è un JtollBar di MaterialtoolBar");
+			return;
+		}
+		super.installUI(c);
+		if(DEBUG) System.out.println("Ok mi trovo nell istallUI di MaterialtoolBar");
+		JToolBar toolBar = (JToolBar)c;
+		toolBar.setFont(MaterialFonts.REGULAR);
+		toolBar.setBackground(MaterialColors.WHITE);
+		toolBar.setForeground(MaterialColors.BLACK);
+		toolBar.setBorder (BorderFactory.createCompoundBorder(MaterialBorders.LIGHT_SHADOW_BORDER, BorderFactory.createEmptyBorder(2, 2, 2, 2)));
+		
+		//Cosa sto facendo ora??
+		this.dockingBorderColor = null;
+		this.floatingBorderColor = null;
+		this.dockingColor = MaterialColors.LIGHT_GREEN_A100;
+		this.floatingColor = MaterialColors.GRAY_200;
+	}
+
+	@Override
+	public void paint(Graphics g, JComponent c) {
+		super.paint(MaterialDrawingUtils.getAliasedGraphics(g), c);
+>>>>>>> Revert "github conflict resolution"
 	}
 }
