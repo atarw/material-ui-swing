a11b825c5961d89f9fdfeec505380bba4727a379
atharva washimkar <atarw@users.noreply.github.com>
2018-08-13 09:46:39 -0400

    Revert "add support jprogressbar"

--- src/mdlaf/components/combobox/MaterialComboBoxUI.java
+++ src/mdlaf/components/combobox/MaterialComboBoxUI.java
@@ -1,65 +1,50 @@
 package mdlaf.components.combobox;
 
-import java.awt.Cursor;
-import java.awt.Graphics;
-import java.awt.Rectangle;
-
-import javax.swing.BorderFactory;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JComponent;
-import javax.swing.plaf.ComponentUI;
-import javax.swing.plaf.basic.BasicComboBoxUI;
-
-import com.sun.corba.se.impl.protocol.BootstrapServerRequestDispatcher;
-
-import mdlaf.resources.MaterialBorders;
-import mdlaf.resources.MaterialColors;
 import mdlaf.resources.MaterialDrawingUtils;
-import mdlaf.resources.MaterialFonts;
-import mdlaf.resources.MaterialImages;
 
-/**
- * 
- * @author https://github.com/vincenzopalazzo
- *
- */
+import javax.swing.*;
+import javax.swing.plaf.ComponentUI;
+import javax.swing.plaf.basic.BasicArrowButton;
+import javax.swing.plaf.basic.BasicComboBoxUI;
+import java.awt.*;
 
-public class MaterialComboBoxUI extends BasicComboBoxUI{
-	
-	public static ComponentUI createUI(JComponent c) {
-		return new MaterialComboBoxUI();
+public class MaterialComboBoxUI extends BasicComboBoxUI {
+
+	public static ComponentUI createUI (JComponent c) {
+		return new MaterialComboBoxUI ();
 	}
 
 	@Override
-	public void installUI(JComponent c) {
-		super.installUI(c);
+	public void installUI (JComponent c) {
+		super.installUI (c);
+
 		JComboBox<?> comboBox = (JComboBox<?>) c;
-		comboBox.setFont(MaterialFonts.BLACK_ITALIC);
-		comboBox.setBackground(MaterialColors.GRAY_300);
-		comboBox.setForeground(MaterialColors.GRAY_800);
-		comboBox.setBorder(BorderFactory.createEmptyBorder());
-		comboBox.setRenderer(new MaterialComboBoxRender());
-	}	
+		comboBox.setFont (UIManager.getFont ("ComboBox.font"));
+		comboBox.setBackground (UIManager.getColor ("ComboBox.background"));
+		comboBox.setForeground (UIManager.getColor ("ComboBox.foreground"));
+		comboBox.setBorder (UIManager.getBorder ("ComboBox.border"));
+		comboBox.setLightWeightPopupEnabled (true);
+		comboBox.setRenderer (new MaterialComboBoxRenderer ());
+	}
 
 	@Override
-	protected JButton createArrowButton() {
-		JButton button = new JButton(new ImageIcon(MaterialImages.DOWN_ARROW));
-		button.setOpaque(true);
-		button.setBorder(BorderFactory.createEmptyBorder());
-		button.setBackground(MaterialColors.GRAY_300);
+	protected JButton createArrowButton () {
+		Icon icon = UIManager.getIcon("ComboBox.buttonIcon");
+		JButton button;
+		if (icon != null) {
+			button = new JButton(icon);
+		} else {
+			button = new BasicArrowButton(SwingConstants.SOUTH);
+		}
+		button.setOpaque (true);
+		button.setBackground (UIManager.getColor ("ComboBox.buttonBackground"));
+		button.setBorder (BorderFactory.createEmptyBorder ());
+
 		return button;
 	}
 
 	@Override
-	public void paint(Graphics g, JComponent c) {
-		super.paint(MaterialDrawingUtils.getAliasedGraphics(g), c);
+	public void paint (Graphics g, JComponent c) {
+		super.paint (MaterialDrawingUtils.getAliasedGraphics (g), c);
 	}
-	
-	
-	
-	
-	
-	
 }
