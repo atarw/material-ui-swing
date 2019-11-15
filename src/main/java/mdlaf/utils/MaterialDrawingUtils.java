c4c41111276161db138d7d17c20e296d93a4edf6
Vincent <vincent.hyvert@sfr.fr>
2019-11-16 00:08:30 +0100

    Update MaterialDrawingUtils.java

--- src/main/java/mdlaf/utils/MaterialDrawingUtils.java
+++ src/main/java/mdlaf/utils/MaterialDrawingUtils.java
@@ -15,19 +15,20 @@ public class MaterialDrawingUtils {
     }
 
     public static Graphics getAliasedGraphics(Graphics g) {
-        Map<RenderingHints.Key, Object> hints = (Map<RenderingHints.Key, Object>) Toolkit.getDefaultToolkit()
-                .getDesktopProperty("awt.font.desktophints");
-        if (hints != null) {
-            hints.put(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-            hints.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_DEFAULT);
-            hints.put(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_DEFAULT);
-            hints.put(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_LCD_HRGB);
-        }
-        Graphics2D g2d = (Graphics2D) g;
-        if (hints != null)
-            g2d.addRenderingHints(hints);
-        // g2d.addRenderingHints (new RenderingHints (RenderingHints.KEY_ANTIALIASING,
-        // RenderingHints.VALUE_ANTIALIAS_ON));
-        return g2d;
+        Map<RenderingHints.Key, Object> hints = (Map<RenderingHints.Key, Object>) Toolkit.getDefaultToolkit ().getDesktopProperty ("awt.font.desktophints");
+
+		if(hints != null){
+			hints.put(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+			hints.put(RenderingHints.KEY_RENDERING,	RenderingHints.VALUE_RENDER_DEFAULT);
+			hints.put(RenderingHints.KEY_COLOR_RENDERING,	RenderingHints.VALUE_COLOR_RENDER_DEFAULT);
+			hints.put(RenderingHints.KEY_TEXT_ANTIALIASING,	RenderingHints.VALUE_TEXT_ANTIALIAS_LCD_HRGB);
+
+			Graphics2D g2d = (Graphics2D) g;
+			g2d.addRenderingHints(hints);
+			return g2d;
+		}
+		
+		//g2d.addRenderingHints (new RenderingHints (RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON));
+		return g;
     }
 }
